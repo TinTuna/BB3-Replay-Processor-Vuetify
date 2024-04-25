bcd5de8bd591fa92a00160546aee42f41a85d809
Rob English <rob.english@live.com>
2024-04-25 23:26:43 +0100

    chore: add new logo guids chore: add inducement type comments

--- src/types/IdTypes/InducementIdTypes.ts
+++ src/types/IdTypes/InducementIdTypes.ts
@@ -1,17 +1,17 @@
 export type InducementIdTypes =
-  | "1" //
-  | "2" //
-  | "3" //
-  | "4" //
-  | "5" //
+  | "1" // Bloodweiser Keg
+  | "2" // Bribe
+  | "3" // Extra Team Training
+  | "4" // Halfling Master Chef
+  | "5" // Wanderind Apothecary
   | "6" // Journeyman Player
   | "7" // Star Player
-  | "8" //
-  | "9" //
-  | "10" //
-  | "11" //
-  | "12" //
+  | "8" // Wizard
+  | "9" // 
+  | "10" // 
+  | "11" // Temp Agency Cheerleader
+  | "12" // Assistant Coach
   | "13" //
   | "14" //
-  | "15" //
+  | "15" // Biased Referee
   | "16"; //

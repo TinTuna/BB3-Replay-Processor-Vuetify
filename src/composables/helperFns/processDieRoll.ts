d9f4cb0939fe7c83e5bd82e7d2253f8782913d83
Robe <rob.english@live.com>
2025-11-02 20:04:23 +0000

    feat: expand ability to process dice types

--- src/composables/helperFns/processDieRoll.ts
+++ src/composables/helperFns/processDieRoll.ts
@@ -12,6 +12,14 @@ export const processDieRoll = (opts: {
       // its a D6
       processD6Roll(opts.dieRoll, opts.playerId, opts.matchData);
       break;
+    case "2":
+      // its a block die
+      processBlockDieRoll(opts.dieRoll, opts.playerId, opts.matchData);
+      break;
+    case "4":
+      // its a 2d6 roll (values 2-12, used for injury table, prayers, etc)
+      process2D6Roll(opts.dieRoll, opts.playerId, opts.matchData);
+      break;
     default:
       break;
   }
@@ -51,4 +59,58 @@ const processD6Roll = (
       break;
     }
   }
+};
+
+const processBlockDieRoll = (
+  dieRoll: Die,
+  playerId: PlayerId,
+  matchData: MatchData
+) => {
+  switch (dieRoll.Value) {
+    case "1": {
+      // Attacker Down
+      matchData.playerData[playerId].blockDiceRolled.attackerDown += 1;
+      break;
+    }
+    case "2": {
+      // Both Down
+      matchData.playerData[playerId].blockDiceRolled.bothDown += 1;
+      break;
+    }
+    case "3": {
+      // Push
+      matchData.playerData[playerId].blockDiceRolled.push += 1;
+      break;
+    }
+    case "4": {
+      // Push (another type)
+      matchData.playerData[playerId].blockDiceRolled.push += 1;
+      break;
+    }
+    case "5": {
+      // Defender Stumbles
+      matchData.playerData[playerId].blockDiceRolled.defenderStumbles += 1;
+      break;
+    }
+    case "6": {
+      // Defender Down
+      matchData.playerData[playerId].blockDiceRolled.defenderDown += 1;
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+};
+
+const process2D6Roll = (
+  dieRoll: Die,
+  playerId: PlayerId,
+  matchData: MatchData
+) => {
+  // 2d6 rolls produce values from 2-12 (or potentially higher for prayers)
+  // These are typically used for injury rolls, prayers, etc.
+  // Currently we don't have a specific storage structure for these individual values
+  // They are tracked as part of the specific roll type (armourRolls, injuryRolls, etc.)
+  // So for now, this is a placeholder for future expansion
 };
--- src/types/IdTypes/DieType.ts
+++ src/types/IdTypes/DieType.ts
@@ -1,6 +1,6 @@
-export type DieType = 
-    | "0" // a standard d6
-    | "1" //
-    | "2" // a block die
-    | "3" //
-    | "4" // 2d6 (2-12)(Used in injury table and maybe other places) ????? This is used in prayers and it gave a result of 16
+export type DieType =
+  | "0" // a standard d6 (values 1-6)
+  | "1" // unknown die type
+  | "2" // a block die (values 1-6: attacker down, both down, push, push, defender stumbles, defender down)
+  | "3" // unknown die type
+  | "4"; // 2d6 roll (values 2-12, or higher for prayers - used in injury table, prayers, etc.)
